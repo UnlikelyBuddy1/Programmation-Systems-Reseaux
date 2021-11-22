bd0e129fed927a66c0a4be8c3620b0d41c6350e7
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2021-11-22 15:25:21 +0100

    trying to solve 6bytes not sending

--- server.c
+++ server.c
@@ -31,26 +31,23 @@ int main (int argc, char *argv[]) {
     while(1) {
         printf("[INFO] Waiting for message being name of file to send ...\n");
         n = recvfrom(udp_data, (char *)buffer_data, RCVSIZE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len); 
-        buffer_data[n] = '\0';
         file = verifyFile(buffer_data, sizeof(buffer_data));
         size_t length = getLengthFile(file);
         char buffer2_lecture[length];
-        
         if((fread(buffer2_lecture,sizeof(char),length,file))!=length){printf("[ERR] Failed to read file\n");} 
         fclose(file);
         nFrags = getNumberFragments(length, pLastFrag);
-        buffer_data[n] = '\0';
-
-        for(int seqNumber=0;seqNumber < nFrags;seqNumber++){
+       
+        for(int seqNumber=0;seqNumber <= nFrags;seqNumber++){
             memcpy(buffer_data+6, buffer2_lecture+((RCVSIZE-6)*seqNumber), RCVSIZE-6);
+            printf();
             snprintf((char *) StringSeqNumber, 7 , "%6d", seqNumber );
             memcpy(buffer_data, StringSeqNumber, 6);
             printf("[INFO] Sending file %d/%d ...",seqNumber, nFrags-1);
             ack = 0;
             while(ack == 0){
                 sendto(udp_data,(const char*)buffer_data, (seqNumber==nFrags)?(lastFragSize):(sizeof(buffer_data)),MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
-                n = recvfrom(udp_data, (char *)buffer_data, RCVSIZE, MSG_WAITALL, (struct sockaddr *) &cliaddr,&len); 
-                buffer_data[n] = '\0';   
+                n = recvfrom(udp_data, (char *)buffer_data, RCVSIZE, MSG_WAITALL, (struct sockaddr *) &cliaddr,&len);   
                 if(strstr(buffer_data, "ACK") != NULL) {
                     if(atoi(strtok(buffer_data,"ACK")) == seqNumber){
                         ack = 1;
@@ -62,11 +59,10 @@ int main (int argc, char *argv[]) {
                 }
             }
         }
-        // ToDo windows
+        printf("\n");
         sendto(udp_data,"FIN", strlen("FIN"),MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
         printf("[INFO] Waiting for message FINAL ACK to end connection ...\n");
         n = recvfrom(udp_data, (char *)buffer_data, RCVSIZE,MSG_WAITALL, (struct sockaddr *) &cliaddr,&len); 
-        buffer_data[n] = '\0';
         printf("[OK] Received FINAL ACK\n");
         printf("there have been %d errors in %d messages\n", errors, nFrags);
     }        
