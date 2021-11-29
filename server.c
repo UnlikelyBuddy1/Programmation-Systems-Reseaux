51e5bfb63c1988aa984d4ebff8e43fbf738e73a2
H4ns906 <reda.m.elmalki@gmail.com>
2021-11-29 11:11:32 +0100

    RTT estimation is a priori implemented. Code needs some formatting but it works.

--- server.c
+++ server.c
@@ -1,55 +1,90 @@
 #include "tools.h"
+#include <sys/time.h>
+#include <time.h>
 #define RTT 100000
-int main (int argc, char *argv[]) {
-    FILE* file;
+int main(int argc, char *argv[])
+{   struct timespec start, end;
+    float RTT_list[100];
+    float SRTT_list[100];
+    int index = 0;
+
+
+
+    FILE *file;
     struct sockaddr_in cliaddr;
     socklen_t len = sizeof(cliaddr);
     memset(&cliaddr, 0, sizeof(cliaddr));
-    unsigned short port_udp_con, port_udp_data, lastFragSize, *pLastFrag=&lastFragSize, nFrags, errors=0, toSend=0;
-    unsigned short *pport_udp_con= &port_udp_con, *pport_udp_data= &port_udp_data, ack = 0;// les deux port UDP utilise
-    char buffer_con[RCVSIZE], buffer_data[RCVSIZE], buffer_ack[10],buffer_file[RCVSIZE-6]; // le buffer_con ou on va recevoir des donnees et des connexion
-    int udp_con, udp_data, n=0;
+    unsigned short port_udp_con, port_udp_data, lastFragSize, *pLastFrag = &lastFragSize, nFrags, errors = 0, toSend = 0;
+    unsigned short *pport_udp_con = &port_udp_con, *pport_udp_data = &port_udp_data, ack = 0; // les deux port UDP utilise
+    char buffer_con[RCVSIZE], buffer_data[RCVSIZE], buffer_ack[10], buffer_file[RCVSIZE - 6]; // le buffer_con ou on va recevoir des donnees et des connexion
+    int udp_con, udp_data, n = 0;
     verifyArguments(argc, argv, pport_udp_con, pport_udp_data);
     udp_con = createSocket();
     udp_con = bindSocket(udp_con, port_udp_con);
     udp_data = createSocket();
     udp_data = bindSocket(udp_data, port_udp_data);
     TWH(udp_con, n, buffer_con, port_udp_data, cliaddr, len);
-    struct timeval tv = setTimer(0,10000);
-    setsockopt(udp_data, SOL_SOCKET, SO_RCVTIMEO, &tv,sizeof(tv));
-    while(1){
-        printf("[INFO] Waiting for message being name of file to send ...\n");    //verifyContents(buffer_data, buffer_file, 0, 20);
-        n = recvfrom(udp_data, (char *)buffer_data, RCVSIZE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+    struct timeval tv = setTimer(0, 10000);
+    setsockopt(udp_data, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+    while (1)
+    {
+        printf("[INFO] Waiting for message being name of file to send ...\n"); //verifyContents(buffer_data, buffer_file, 0, 20);
+        n = recvfrom(udp_data, (char *)buffer_data, RCVSIZE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
         file = verifyFile(buffer_data, sizeof(buffer_data));
         size_t length = getLengthFile(file);
         nFrags = getNumberFragments(length, pLastFrag);
-        for(int seqNumber=1;seqNumber < nFrags+1;seqNumber++){
-            toSend=fread(buffer_file, 1, (seqNumber==nFrags)?(lastFragSize):(RCVSIZE-6), file);
+        for (int seqNumber = 1; seqNumber < nFrags + 1; seqNumber++)
+        {
+            toSend = fread(buffer_file, 1, (seqNumber == nFrags) ? (lastFragSize) : (RCVSIZE - 6), file);
             sprintf(buffer_data, "%6d", seqNumber);
-            memcpy(buffer_data+6, buffer_file, sizeof(buffer_file));
+            memcpy(buffer_data + 6, buffer_file, sizeof(buffer_file));
             printf("[INFO] Sending file %d/%d ...\r", seqNumber, nFrags);
             ack = 0;
-            while(ack == 0){
-                sendto(udp_data,(const char*)buffer_data, toSend+6, MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
-                n = recvfrom(udp_data, (char *)buffer_ack, sizeof(buffer_ack), MSG_WAITALL, (struct sockaddr *) &cliaddr,&len);   
-                if(strstr(buffer_ack, "ACK") != NULL) {
-                    if(atoi(strtok(buffer_ack,"ACK")) == seqNumber){
+            float SRTT_0 = 4;
+            float diff = 0;
+            while (ack == 0)
+            {
+                sendto(udp_data, (const char *)buffer_data, toSend + 6, MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
+                clock_gettime(CLOCK_REALTIME, &start);
+
+                n = recvfrom(udp_data, (char *)buffer_ack, sizeof(buffer_ack), MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+                clock_gettime(CLOCK_REALTIME, &end);
+                time_converter(start, end, index, RTT_list);
+                SRTT_list[index] = RTT_ESTIMATION(0.2, SRTT_0, RTT_list[index]);
+                diff = SRTT_list[index] - RTT_list[index]; 
+                printf("RTT (%9.6f) vs SRTT (%9.6f) and the difference is : (%9.6f)\n",RTT_list[index], SRTT_list[index], diff);
+                SRTT_0 = SRTT_list[index];
+
+                //printf("The RTT value of this instance of exchange is : %9.6f\n", RTT_list[index]);
+
+
+
+                if (strstr(buffer_ack, "ACK") != NULL)
+                {
+                    if (atoi(strtok(buffer_ack, "ACK")) == seqNumber)
+                    {
                         ack = 1;
                     }
-                }else{
-                    errors+=1;}}
+                }
+                else
+                {
+                    errors += 1;
+                }
+            }
             memset(buffer_data, 0, RCVSIZE);
             memset(buffer_ack, 0, sizeof(buffer_ack));
         }
         printf("\n");
-        sendto(udp_data,"FIN", strlen("FIN"),MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
+        sendto(udp_data, "FIN", strlen("FIN"), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
         printf("[INFO] Waiting for message FINAL ACK to end connection ...\n");
-        n = recvfrom(udp_data, (char *)buffer_ack, RCVSIZE,MSG_WAITALL, (struct sockaddr *) &cliaddr,&len);
-        if(strstr(buffer_ack, "ACK") != NULL) {
+        n = recvfrom(udp_data, (char *)buffer_ack, RCVSIZE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+        if (strstr(buffer_ack, "ACK") != NULL)
+        {
             printf("[OK] Received FINAL ACK\n");
-            memset(buffer_ack, 0, sizeof(buffer_ack));}
+            memset(buffer_ack, 0, sizeof(buffer_ack));
+        }
         fclose(file);
         printf("[INFO] There have been %d errors in %d messages\n", errors, nFrags);
-    } 
+    }
     return 0;
 }
--- tools.h
+++ tools.h
@@ -11,141 +11,183 @@
 #define RCVSIZE 1024
 #define LOG 1
 
-char pass(){
+char pass()
+{
     return ' ';
 }
-void verifyArguments(int argc, char *argv[], unsigned short *port_udp_con, unsigned short *port_udp_data){
-    if(argc > 2){
+float RTT_ESTIMATION(float alpha, float SRTT_value, float RTT_value)
+{
+    return alpha * SRTT_value + (1 - alpha) * RTT_value;
+}
+
+void time_converter(struct timespec start, struct timespec end, int index, float RTT_list[])
+{
+    float sec_to_milli = (float)((end.tv_sec - start.tv_sec) * 1000);
+    float nano_to_milli = (float)((end.tv_nsec - start.tv_nsec) / (1000000));
+    RTT_list[index] = sec_to_milli + nano_to_milli;
+    
+   
+}
+void verifyArguments(int argc, char *argv[], unsigned short *port_udp_con, unsigned short *port_udp_data)
+{
+    if (argc > 2)
+    {
         perror("[ERR] Too many arguments\n");
         exit(0);
     }
-    if(argc < 2){
+    if (argc < 2)
+    {
         perror("[ERR] Argument expected\n");
         exit(0);
     }
-    if(argc == 2){
+    if (argc == 2)
+    {
         int n = 1;
         *port_udp_con = atoi(argv[1]);
-        *port_udp_data = atoi(argv[1])+n;
+        *port_udp_data = atoi(argv[1]) + n;
         n++;
     }
-    (LOG)?(printf("[OK] Ports used are %d and %d\n", *port_udp_con, *port_udp_data)):(pass());
+    (LOG) ? (printf("[OK] Ports used are %d and %d\n", *port_udp_con, *port_udp_data)) : (pass());
 }
 
-int createSocket(){
+int createSocket()
+{
     int udpSocket;
-    udpSocket=socket(AF_INET, SOCK_DGRAM, 0);
-    if(udpSocket < 0){
+    udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udpSocket < 0)
+    {
         perror("[ERR] Cannot create udp socket\n");
         exit(0);
-    } else{
-        (LOG)?(printf("[OK] Socket server UDP created %d\n", udpSocket) ):(pass());
+    }
+    else
+    {
+        (LOG) ? (printf("[OK] Socket server UDP created %d\n", udpSocket)) : (pass());
     }
     return udpSocket;
 }
 
-int bindSocket(int udpSocket, unsigned short port_udp){
+int bindSocket(int udpSocket, unsigned short port_udp)
+{
     struct sockaddr_in adresse;
-    memset(&adresse, 0, sizeof(adresse)); 
-    adresse.sin_family= AF_INET;
-    adresse.sin_port= htons(port_udp);    
-    adresse.sin_addr.s_addr= INADDR_ANY;
-    (LOG)?(printf("[OK] Created adresse with port %d and addr %d\n", adresse.sin_port, adresse.sin_addr.s_addr)):(pass());
-    if (bind(udpSocket, (struct sockaddr*) &adresse, sizeof(adresse))<0) {
+    memset(&adresse, 0, sizeof(adresse));
+    adresse.sin_family = AF_INET;
+    adresse.sin_port = htons(port_udp);
+    adresse.sin_addr.s_addr = INADDR_ANY;
+    (LOG) ? (printf("[OK] Created adresse with port %d and addr %d\n", adresse.sin_port, adresse.sin_addr.s_addr)) : (pass());
+    if (bind(udpSocket, (struct sockaddr *)&adresse, sizeof(adresse)) < 0)
+    {
         perror("[ERR] Bind failed\n");
         close(udpSocket);
         return -1;
     }
-    (LOG)?(printf("[OK] Binded server %d to adress with family %d, port %d, addr %d\n",udpSocket, adresse.sin_family, adresse.sin_port,adresse.sin_addr.s_addr)):(pass());
+    (LOG) ? (printf("[OK] Binded server %d to adress with family %d, port %d, addr %d\n", udpSocket, adresse.sin_family, adresse.sin_port, adresse.sin_addr.s_addr)) : (pass());
     return udpSocket;
 }
 
-void TWH(int udpSocket, int n, char buffer[], unsigned short port_udp, struct sockaddr_in cliaddr, socklen_t len){
-    (LOG)?(printf("[INFO] Begining three way handshake waiting to receive SYN(blocking)\n")):(pass());
-    n = recvfrom(udpSocket, (char *)buffer, RCVSIZE,MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+void TWH(int udpSocket, int n, char buffer[], unsigned short port_udp, struct sockaddr_in cliaddr, socklen_t len)
+{
+    (LOG) ? (printf("[INFO] Begining three way handshake waiting to receive SYN(blocking)\n")) : (pass());
+    n = recvfrom(udpSocket, (char *)buffer, RCVSIZE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
     buffer[n] = '\0';
-    (LOG)?(printf("[OK] buffer_con to receive created %d\n", n)):(pass());
-    
-    if(strcmp(buffer,"SYN")==0){
-        (LOG)?(printf("[INFO] The buffer_con is :\n")):(pass());
-        (LOG)?(printf("[DATA] %s\n", buffer)):(pass());
+    (LOG) ? (printf("[OK] buffer_con to receive created %d\n", n)) : (pass());
+
+    if (strcmp(buffer, "SYN") == 0)
+    {
+        (LOG) ? (printf("[INFO] The buffer_con is :\n")) : (pass());
+        (LOG) ? (printf("[DATA] %s\n", buffer)) : (pass());
         char synack[11];
         char port_udp_data_s[6];
         strcpy(synack, "SYN-ACK");
-        snprintf((char *) port_udp_data_s, 10 , "%d", port_udp ); 
-        strcat(synack,port_udp_data_s);
+        snprintf((char *)port_udp_data_s, 10, "%d", port_udp);
+        strcat(synack, port_udp_data_s);
         strcpy(buffer, synack);
-        (LOG)?(printf("[INFO] Sending for SYN-ACK...\n")):(pass());
-        (LOG)?(printf("[INFO] The buffer_con is :\n")):(pass());
-        (LOG)?(printf("[DATA] %s\n", buffer)):(pass());
+        (LOG) ? (printf("[INFO] Sending for SYN-ACK...\n")) : (pass());
+        (LOG) ? (printf("[INFO] The buffer_con is :\n")) : (pass());
+        (LOG) ? (printf("[DATA] %s\n", buffer)) : (pass());
         // En multi client il faut creer le thread avant de envoyer le synack
         // faire lecture fichier -> envoi -> réecriture
-        sendto(udpSocket, (char *)buffer, strlen(buffer),MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-        (LOG)?(printf("[INFO] Waiting for ACK ...\n")):(pass());
-        n = recvfrom(udpSocket, (char *)buffer, RCVSIZE,MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+        sendto(udpSocket, (char *)buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
+        (LOG) ? (printf("[INFO] Waiting for ACK ...\n")) : (pass());
+        n = recvfrom(udpSocket, (char *)buffer, RCVSIZE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
         buffer[n] = '\0';
-        (LOG)?(printf("[INFO] The buffer_con is :\n")):(pass());
-        (LOG)?(printf("[DATA] %s\n", buffer)):(pass());
-        if(strcmp(buffer,"ACK")==0){
-            (LOG)?(printf("[OK] ACK received, succesful connection\n")):(pass());
-        // Handshake reussi !
-
-        }else{
-        printf("[ERR] Bad ack");
-        exit(0);
+        (LOG) ? (printf("[INFO] The buffer_con is :\n")) : (pass());
+        (LOG) ? (printf("[DATA] %s\n", buffer)) : (pass());
+        if (strcmp(buffer, "ACK") == 0)
+        {
+            (LOG) ? (printf("[OK] ACK received, succesful connection\n")) : (pass());
+            // Handshake reussi !
         }
-    }else{
+        else
+        {
+            printf("[ERR] Bad ack");
+            exit(0);
+        }
+    }
+    else
+    {
         printf("[ERR] Bad SYN");
         exit(0);
     }
 }
 
-size_t getLengthFile(FILE *file){
+size_t getLengthFile(FILE *file)
+{
     size_t pos = ftell(file);    // Current position
     fseek(file, 0, SEEK_END);    // Go to end
     size_t length = ftell(file); // read the position which is the size
     fseek(file, pos, SEEK_SET);
-    (LOG)?(printf("[INFO] File has a size of %lu bytes\n",length)):(pass());
+    (LOG) ? (printf("[INFO] File has a size of %lu bytes\n", length)) : (pass());
     return length;
 }
 
-struct timeval setTimer(unsigned int seconds, unsigned int micro_seconds){
+struct timeval setTimer(unsigned int seconds, unsigned int micro_seconds)
+{
     struct timeval tv;
     tv.tv_sec = seconds;
     tv.tv_usec = micro_seconds;
     return tv;
 }
 
-unsigned short getNumberFragments(size_t length, unsigned short *pLastFrag){
+unsigned short getNumberFragments(size_t length, unsigned short *pLastFrag)
+{
     unsigned short numberFrags;
-    if((length % (RCVSIZE-6)) != 0){
-        numberFrags = (length/(RCVSIZE-6))+1;
-        *pLastFrag = (length % (RCVSIZE-6));
-    } else {
-        numberFrags = length/(RCVSIZE-6);
-    }        
-    (LOG)?(printf("[OK] There needs to be %d fragments\n",numberFrags)):(pass());
-    (LOG)?(printf("[INFO] size at the end will be %d\n", *pLastFrag)):(pass());
+    if ((length % (RCVSIZE - 6)) != 0)
+    {
+        numberFrags = (length / (RCVSIZE - 6)) + 1;
+        *pLastFrag = (length % (RCVSIZE - 6));
+    }
+    else
+    {
+        numberFrags = length / (RCVSIZE - 6);
+    }
+    (LOG) ? (printf("[OK] There needs to be %d fragments\n", numberFrags)) : (pass());
+    (LOG) ? (printf("[INFO] size at the end will be %d\n", *pLastFrag)) : (pass());
     return numberFrags;
 }
 
-FILE *verifyFile(char buffer[], int size){
+FILE *verifyFile(char buffer[], int size)
+{
     FILE *file;
     char filename[size];
-        strcpy(filename, buffer);
-        (LOG)?(printf("[OK] Message is : %s\n", buffer)):(pass());
-        file = NULL;
-        if((file=fopen(filename,"r+"))==NULL){
-            printf("[ERR] File %s does not exist\n", filename);
-            exit(-1);
-        } else {
-            (LOG)?(printf("[OK] Found file %s\n", filename)):(pass());
-        }
+    strcpy(filename, buffer);
+    (LOG) ? (printf("[OK] Message is : %s\n", buffer)) : (pass());
+    file = NULL;
+    if ((file = fopen(filename, "r+")) == NULL)
+    {
+        printf("[ERR] File %s does not exist\n", filename);
+        exit(-1);
+    }
+    else
+    {
+        (LOG) ? (printf("[OK] Found file %s\n", filename)) : (pass());
+    }
     return file;
 }
 
-void verifyContents(char buffer[], char buffer2[], unsigned int start, unsigned int end){
-    for(unsigned int i=start; i<end; i++){
+void verifyContents(char buffer[], char buffer2[], unsigned int start, unsigned int end)
+{
+    for (unsigned int i = start; i < end; i++)
+    {
         printf("content is : src %x dest %x\n", buffer[i], buffer2[i]);
     }
 }
